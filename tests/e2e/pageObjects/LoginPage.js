1c6d8a8fbad77c9233bbf2bffe9f4d026d5998b9
KarunAtreya <karunat1999@gmail.com>
2023-06-21 10:12:13 +0545

    lint ci

--- tests/e2e/pageObjects/LoginPage.js
+++ tests/e2e/pageObjects/LoginPage.js
@@ -1,26 +1,26 @@
-const { tandoorURL } = require("../../cucumber.conf.js");
+const { tandoorURL } = require('../../cucumber.conf.js')
 
 class LoginPage {
-  constructor() {
-    this.loginPageUrl = tandoorURL + "accounts/login/";
-    this.usernameInputSelector = "//input[@id='id_login']";
-    this.passwordInputSelector = "//input[@id='id_password']";
-    this.signinButtonSelector = "//div[@id='div_id_password']/following-sibling::button[contains(@class,'btn-success')]";
-    this.loginFailMessageSelector = "//div[contains(@class,'alert-danger')]//li";
+  constructor () {
+    this.loginPageUrl = tandoorURL + 'accounts/login/'
+    this.usernameInputSelector = "//input[@id='id_login']"
+    this.passwordInputSelector = "//input[@id='id_password']"
+    this.signinButtonSelector = "//div[@id='div_id_password']/following-sibling::button[contains(@class,'btn-success')]"
+    this.loginFailMessageSelector = "//div[contains(@class,'alert-danger')]//li"
   }
 
-  async login(username, password) {
-    await page.locator(this.usernameInputSelector).fill(username);
-    await page.locator(this.passwordInputSelector).fill(password);
-    await page.locator(this.signinButtonSelector).click();
+  async login (username, password) {
+    await page.locator(this.usernameInputSelector).fill(username)
+    await page.locator(this.passwordInputSelector).fill(password)
+    await page.locator(this.signinButtonSelector).click()
   }
 
-  getMessage() {
-    return page.locator(this.loginFailMessageSelector).innerText();
+  getMessage () {
+    return page.locator(this.loginFailMessageSelector).innerText()
   }
 
-  async goToLoginPage() {
-    await page.goto(this.loginPageUrl);
+  async goToLoginPage () {
+    await page.goto(this.loginPageUrl)
   }
 }
 
